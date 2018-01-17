f0061d1918305d65b02b700acaa34b8040b66613
Khaled (Kal) Henidak <khnidk@outlook.com>
2018-01-16 16:41:41 -0800

    revise versions on short cuts

--- tools/dysk_shortcuts.sh
+++ tools/dysk_shortcuts.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-alias install_dysk="docker run --rm -it --privileged -v /usr/src:/usr/src  -v /lib/modules:/lib/modules khenidak/dysk-installer:1.0"
-alias dyskctl="docker run --rm -it --privileged -v /etc/ssl/certs:/etc/ssl/certs:ro khenidak/dysk-cli:0.0.1"
+alias install_dysk="docker run --rm -it --privileged -v /usr/src:/usr/src  -v /lib/modules:/lib/modules khenidak/dysk-installer:0.1"
+alias dyskctl="docker run --rm -it --privileged -v /etc/ssl/certs:/etc/ssl/certs:ro khenidak/dysk-cli:0.1"
